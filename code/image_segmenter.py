7d68cab0904913ccc8c953657ad7c6cc504ffcd2
Noa Ette <noa.ette@epfl.ch>
2024-11-20 11:03:58 +0100

    Implemented realtime image segmentation on esp32-cam feed

---
+++ code/image_segmenter.py
@@ -0,0 +1,80 @@
+import torch
+import numpy as np
+import cv2
+from sam2 import build_sam
+# use bfloat16 for the entire notebook
+torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
+
+if torch.cuda.get_device_properties(0).major >= 8:
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+
+class ImageSegmenter:
+    def __init__(self, model_cfg, checkpoint, expand_pixels=0):
+        self.predictor = build_sam.build_sam2_camera_predictor(model_cfg, checkpoint)
+        self.if_init = False
+        self.expand_pixels = expand_pixels
+
+    def initialize(self, frame, points=None, bbox=None):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.width, self.height = frame.shape[:2][::-1]
+
+        self.predictor.load_first_frame(frame)
+        self.if_init = True
+
+        ann_frame_idx = 0  # the frame index we interact with
+        ann_obj_id = 1  # give a unique id to each object we interact with (it can be any integers)
+
+        if points is not None:
+            labels = np.array([1] * len(points), dtype=np.int32)
+            _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
+                frame_idx=ann_frame_idx, obj_id=ann_obj_id, points=points, labels=labels
+            )
+        elif bbox is not None:
+            _, self.out_obj_ids, self.out_mask_logits = self.predictor.add_new_prompt(
+                frame_idx=ann_frame_idx, obj_id=ann_obj_id, bbox=bbox
+            )
+
+    def propagate(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out_obj_ids, out_mask_logits = self.predictor.track(frame)
+
+        all_mask = np.zeros((self.height, self.width, 1), dtype=np.uint8)
+        for i in range(0, len(out_obj_ids)):
+            out_mask = (out_mask_logits[i] > 0.0).permute(1, 2, 0).cpu().numpy().astype(
+                np.uint8
+            ) * 255
+            if self.expand_pixels > 0:
+                kernel = np.ones((self.expand_pixels, self.expand_pixels), np.uint8)
+                out_mask = cv2.dilate(out_mask, kernel, iterations=1)
+            all_mask = cv2.bitwise_or(all_mask, out_mask)
+
+        return cv2.cvtColor(all_mask, cv2.COLOR_GRAY2RGB)
+    
+if __name__ == "__main__":
+
+    scanner = ImageSegmenter(model_cfg="sam2_hiera_s.yaml", checkpoint="sam2_checkpoints/sam2_hiera_small.pt", expand_pixels=10)
+
+    cap = cv2.VideoCapture("/home/noaemien/Downloads/knx.mp4")
+
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if not ret:
+            break
+
+        if not scanner.if_init:
+            points = np.array([[608, 253]], dtype=np.float32)
+            scanner.initialize(frame, points=points)
+        else:
+            all_mask = scanner.propagate(frame)
+            all_mask = cv2.cvtColor(all_mask, cv2.COLOR_GRAY2RGB)
+            frame = cv2.addWeighted(frame, 1, all_mask, 1, 0)
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
--- code/main.py
+++ code/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 from webserver import ESP32CAM
 from ui import ImageUI
-from image_segmentation import ImageSegmentation
+from image_segmenter import ImageSegmenter
+import cv2
 
 def main():
     esp32cam = ESP32CAM("http://172.20.10.8:80/stream")
@@ -10,13 +11,14 @@ def main():
     if frame is None or frame.size == 0:
         print("Failed to receive initial frame.")
         return
-
+    
     # Initialize UI with the first frame
     ui = ImageUI(frame)
 
-    # Initialize ImageSegmentation with a random (for now) bounding box
-    bbox = np.array([[100, 150], [100, 150]], dtype=np.float32)
-    segmenter = ImageSegmentation(frame, bbox=bbox)
+    # Initialize ImageSegmenter with a random (for now) bounding box
+    points = np.array([[100, 150]], dtype=np.float32)
+    segmenter = ImageSegmenter(model_cfg="sam2_hiera_s.yaml", checkpoint="sam2_checkpoints/sam2_hiera_small.pt", expand_pixels=10)
+    segmenter.initialize(frame, points=points)
 
     def update_frame():
         frame = esp32cam.get_frame()
@@ -26,10 +28,12 @@ def main():
             return
 
         # Segment the frame
-        segmented_frame = segmenter.propagate(frame)
+        mask = segmenter.propagate(frame)
+
+        frame = cv2.addWeighted(frame, 1, mask, 1, 0)
 
         # Update the UI with the segmented frame
-        ui.update_image(segmented_frame)
+        ui.update_image(frame)
 
     ui.run(update_frame)
 
