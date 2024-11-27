a75bc8e5af46cb540cae0e8a6c7c5020f1b7d133
Ugo <ugo.novello@epfl.ch>
2024-11-27 10:17:53 +0100

    added code tests from project

---
+++ code/esp32-s2 snippets/arm.ino
@@ -0,0 +1,89 @@
+#include <AccelStepper.h>
+
+// Define some steppers and the pins the will use
+int enablePin = 40;
+AccelStepper stepper2(AccelStepper::DRIVER, 37, 36); // the one with belt facing the other way
+AccelStepper stepper1(AccelStepper::DRIVER, 11, 10);
+
+long x = 0;
+long y = 0;
+
+const long a1 = 40; // length of lower arm in cm
+const long a2 = 40; // length of upper arm in cm
+const long full_revolution = 200;
+float q1 = 0; // angle of lower arm (in radians)
+float q2 = 0; // angle of upper arm (in radians)
+
+void setup() {
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, LOW);
+
+  stepper1.setMaxSpeed(1000.0);
+  stepper1.setAcceleration(100.0);
+  
+  stepper2.setMaxSpeed(1000.0);
+  stepper2.setAcceleration(100.0);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("Current Angle : " + String(q1) + ", " + String(q2));
+  Serial.println("Please enter q1 q2 angles (press Enter after typing):");
+
+  while (Serial.available() == 0) {
+    // Wait for user input
+  }
+  q1 = Serial.parseInt() * PI / 180;
+  q2 = Serial.parseInt() * PI / 180;
+  while (Serial.available() > 0) {
+    Serial.read(); // Clear the buffer
+  }
+
+  /*
+  Serial.println("Current Coordinates : " + String(x) + ", " + String(y));
+  Serial.println("Please enter x y coordinates (press Enter after typing):");
+
+  while (Serial.available() == 0) {
+    // Wait for user input
+  }
+  x = Serial.parseInt();
+  y = Serial.parseInt();
+  while (Serial.available() > 0) {
+    Serial.read(); // Clear the buffer
+  }
+
+  if (sqrt(pow(x, 2) + pow(y, 2)) > (a1 + a2) || sqrt(pow(x, 2) + pow(y, 2)) < abs(a1 - a2)) {
+    Serial.println("Error: Coordinates out of range. Please enter valid values.");
+    return;
+  }
+
+  Serial.print("x = ");
+  Serial.print(x);
+  Serial.print("  y = ");
+  Serial.println(y);
+
+  noma_angles();
+  */
+  Serial.println("New angles : " + String(q1) + ", " + String(q2));
+  Serial.println("New angles (degrees) : " + String(q1 * 180 / PI) + ", " + String(q2 * 180 / PI));
+  
+  stepper1.moveTo(full_revolution * q1 / (2 * PI));
+  stepper2.moveTo(-full_revolution * q2 / (2 * PI));
+
+  Serial.println("Moving to : " + String(full_revolution * q1 / (2 * PI)) + ", " + String(full_revolution * q2 / (2 * PI)));
+
+  while(stepper1.distanceToGo() != 0) {
+        stepper1.run();
+        Serial.println(stepper1.speed());
+  }
+  while(stepper2.distanceToGo() != 0) {
+        stepper2.run();
+        Serial.println(stepper2.speed());
+  }
+}
+
+void noma_angles() {
+  q2 = acos((pow(x, 2) + pow(y, 2) - pow(a1, 2) - pow(a2, 2)) / (2 * a1 * a2)); 
+  q1 = atan2(y, x) - atan2(a2 * sin(q2), (a1 + a2 * cos(q2))); 
+}
---
+++ code/esp32-s2 snippets/rest_api/rest_api.ino
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
+#include <FreeRTOS.h>
+
+const char *SSID = "qwer";
+const char *PWD = "12345678";
+
+WebServer server(80);
+
+StaticJsonDocument<250> jsonDocument;
+char buffer[250];
+ 
+void setup_routing() {    
+  server.on("/data", getData);     
+  server.on("/led", HTTP_POST, handlePost);    
+          
+  server.begin();    
+}
+ 
+void create_json(char *tag, float value, char *unit) {  
+  jsonDocument.clear();  
+  jsonDocument["type"] = tag;
+  jsonDocument["value"] = value;
+  jsonDocument["unit"] = unit;
+  serializeJson(jsonDocument, buffer);
+}
+ 
+void add_json_object(char *tag, float value, char *unit) {
+  JsonObject obj = jsonDocument.createNestedObject();
+  obj["type"] = tag;
+  obj["value"] = value;
+  obj["unit"] = unit; 
+}
+
+ 
+void getData() {
+  Serial.println("Get BME280 Sensor Data");
+  server.send(200, "application/json", buffer);
+}
+
+void handlePost() {
+  if (server.hasArg("plain") == false) {
+  }
+  String body = server.arg("plain");
+  deserializeJson(jsonDocument, body);
+
+  int time = jsonDocument["time"];
+
+  Serial.println(time);
+
+  server.send(200, "application/json", "{}");
+}
+
+void setup() {     
+  Serial.begin(115200);  
+
+  Serial.print("Connecting to Wi-Fi");
+  WiFi.begin(SSID, PWD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+ 
+  Serial.print("Connected! IP Address: ");
+  Serial.println(WiFi.localIP()); 
+  setup_routing();     
+   
+}    
+       
+void loop() {    
+  server.handleClient();     
+}
